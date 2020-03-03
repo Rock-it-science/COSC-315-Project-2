0e49a3ebf3d7e5f25d81a194371f4d5b22e01b14
Rock-it-science <wilmc17@gmail.com>
2020-03-02 19:53:57 -0800

    C++ finished

--- C++/main.cpp
+++ C++/main.cpp
@@ -8,6 +8,28 @@
 #include <pthread.h>
 using namespace std;
 
+/* Choose from below presets for different modes 
+*     They change the N value and the max value for sleep functions
+*  Uncomment one block to activate mode
+*/
+
+//Rapid chaos
+// #define N 20
+// #define maxLength 1000
+// #define prodMaxWait 100
+
+//Regular
+#define N 3
+#define maxLength 5000
+#define prodMaxWait 3000
+
+//Slow
+// #define N 3
+// #define maxLength 13000
+// #define prodMaxWait 9000
+
+
+
 //Semaphore setup 
 // Some code from here: https://stackoverflow.com/questions/4792449/c0x-has-no-semaphores-how-to-synchronize-threads
 class Semaphore{
@@ -64,13 +86,13 @@ class BoundedBuffer{
         int idOffset;
         int lengthOffset;
 
-    BoundedBuffer(int N){
+    BoundedBuffer(int n){
         mutex.value = 1;
-        empty.value = N;
+        empty.value = n;
         id = 0;
         full.value = 0;
-        idVec.resize(N);
-        lengthVec.resize(N);
+        idVec.resize(n);
+        lengthVec.resize(n);
         idOffset = 0;
         lengthOffset = 0;
     }
@@ -82,14 +104,13 @@ class BoundedBuffer{
 
         //Generate request and add to queue (increment queueVal and id)
         idVec.insert(idVec.begin()+idOffset, id);
-        lengthVec.insert(lengthVec.begin()+lengthOffset, rand()%10000);
+        lengthVec.insert(lengthVec.begin()+lengthOffset, rand()%maxLength);
         idOffset++;
         lengthOffset++;
         id++;
         const time_t ctt = time(0);
-        std::cout << "Producer added request: ID=" << idVec[idOffset-1] << ", length=" << lengthVec[lengthOffset-1] << "\n";
-        //std::cout << "Producer: produced request ID " << request[0] << " , length " << request[1]/100 << " seconds, current time is " << asctime(localtime(&ctt)) << "\n";
-
+        std::cout << "\033[1;31mProducer\033[0m added request: ID=" << idVec[idOffset-1] << ", length=" << lengthVec[lengthOffset-1] << "\n";
+        
         //Release mutex
         mutex.Signal();
         full.Signal();
@@ -97,7 +118,7 @@ class BoundedBuffer{
         return;
     }
 
-    int * Consumer(){
+    std::array<int, 2> Consumer(){
         //Get mutex
         full.Wait();
         mutex.Wait();
@@ -107,15 +128,14 @@ class BoundedBuffer{
         int itemTime = lengthVec[lengthOffset-1];
         idOffset--;
         lengthOffset--;
-        time_t ctt = time(0);
-        std::cout << "Consumer reading request: ID=" << itemId << ", length=" << itemTime <<"\n";
-        //std::cout << "Consumer " << threadId << ": assigned request ID " << itemId << ", processing request for the next " << itemTime/1000 << " seconds, current time is " << asctime(localtime(&ctt)) << "\n";
+        std::cout << "\033[1;34mConsumer\033[m reading request: ID=" << itemId << ", length=" << itemTime <<"\n";
 
         //Release mutex
         mutex.Signal();
         empty.Signal();
 
-        static int r[2] = {itemId, itemTime};
+        //Return itemdId and itemTime to slave thread
+        std::array<int, 2> r = {itemId, itemTime};
         return r;
     }
     private:
@@ -124,68 +144,66 @@ class BoundedBuffer{
         Semaphore full;
 };
 
-#define N 5
-//        ^ Change this to change value of N
-
 //Global object of boundedBuffer for use in standalone functions
 BoundedBuffer bb(N);
 
 //Called by master threads
 static void *master(void *arg){
-    std::cout << "starting master on thread " << pthread_self() << "\n";
-    //Call producer
-    bb.Producer();
+    std::cout << "Starting master on thread " << pthread_self() << "\n";
 
-    //Sleep for random amount of time (<1 sec) and restart
-    int prodSleep = rand()%10000;
-    std::cout << "Producer: sleeping for " << prodSleep << " ms\n";
-    std::this_thread::sleep_for(std::chrono::milliseconds(prodSleep));
+    //Loop forever
+    while(true){
+        //Call producer
+        bb.Producer();
 
-    //Recursion
-    master(NULL);
+        //Sleep for random amount of time (<1 sec) and restart
+        int prodSleep = rand()%prodMaxWait;
+        std::cout << "Producer: sleeping for " << prodSleep << " ms\n";
+        std::this_thread::sleep_for(std::chrono::milliseconds(prodSleep));
+    }
 }
 
 //Called by slave threads
 static void *slave(void *arg){
-    std::cout << "starting idle slave on thread " << pthread_self() << "\n";
-    bool idle = true;
-            
-    //While queue is empty, be idle
-    while(bb.idOffset == 0){
-        std:this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::cout << "starting idle slave on thread " << pthread_self() << "\n"; //(will sometimes at the same time due to multithreading and this instruction not being run atomically)
+
+    //Loop forever
+    while(true){
+        //While queue is empty, be idle
+        while(bb.idOffset == 0){
+            std:this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+
+        //Queue not empty
+        std::array<int, 2> threadInfo = bb.Consumer();
+        int itemId = threadInfo[0];
+        int itemLength = threadInfo[1];
+        pthread_t threadId = pthread_self();
+
+        //Use item (put to sleep for itemTime)
+        std::this_thread::sleep_for(std::chrono::milliseconds(itemLength));
+        std::cout << "Consumer " << threadId << ": completed request ID " << itemId << "\n";
+        
     }
-
-    cout << "slave thread " << pthread_self() << " no longer idle\n";
-
-    //Queue not empty
-    idle = false;
-    int* threadInfo = bb.Consumer();
-    int itemId = threadInfo[0];
-    int itemLength = threadInfo[1];
-    pthread_t threadId = pthread_self();
-
-    //Use item (put to sleep for itemTime)
-    std::this_thread::sleep_for(std::chrono::milliseconds(itemLength));
-    time_t ctt = time(0);
-    std::cout << "Consumer " << threadId << ": completed request ID " << itemId << " at time " << asctime(localtime(&ctt));
-    
-    pthread_exit(NULL);
 }
 
 int main(){
     pthread_t threads[N];
     int rc;
-    
-    //Run producer on master thread
-    master(NULL);
+    intptr_t id;
 
     //Fork and run consumer on N threads
     for(int i=0; i<N; i++){
-        rc = pthread_create(&threads[i], NULL, slave, (void *)i);
+        id = (intptr_t) i;
+        rc = pthread_create(&threads[i], NULL, slave, (void *)id);
         if(rc){
             cout << "Error in creating thread " << rc << endl;
             exit(-1);
         }
     }
+
+    //Run producer on master thread
+    master(NULL);
+
     pthread_exit(NULL);
 }
